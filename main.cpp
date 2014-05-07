79058105ad03e7f0256de5255d82eccd8375b52e
akhier <akhierthedragonhearted@yahoo.com>
2014-05-07 02:38:20 -0400

    *reorganize things as need to get it working

--- main.cpp
+++ main.cpp
@@ -6,43 +6,6 @@
 
 const int SCREEN_WIDTH = 640, SCREEN_HEIGHT = 480;
 
-int main(int argc, char **argv){
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
-        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    SDL_Window *window = SDL_CreateWindow("LearningSDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (window == nullptr){
-        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (renderer == nullptr){
-        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    SDL_Surface *image = SDL_LoadBMP("sample.bmp");
-    if (image == nullptr){
-        std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, image);
-    SDL_FreeSurface(image);
-    if (texture == nullptr){
-        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() <<std::endl;
-        return 1;
-    }
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(2000);
-    SDL_DestroyTexture(texture);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
-}
-
 void logSDLError(std::ostream &os, const std::string &message){
     os << message << " Error: " << SDL_GetError() << std::endl;
 }
@@ -61,4 +24,31 @@ SDL_Texture* loadBMPTexture(const std::string &file, SDL_Renderer *renderer){
         logSDLError(std::cout, "LoadBMP");
     }
     return texture;
+}
+
+int main(int argc, char **argv){
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        logSDLError(std::cout, "SDL_Init");
+        return 1;
+    }
+    SDL_Window *window = SDL_CreateWindow("LearningSDL", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr){
+        logSDLError(std::cout, "SDL_CreateWindow");
+        return 1;
+    }
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr){
+        logSDLError(std::cout, "SDL_CreateRenderer");
+        return 1;
+    }
+    SDL_Texture *texture = loadBMPTexture("sample.bmp", renderer);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
