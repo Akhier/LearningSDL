5de3c61375c75927c8f5a2da31fa04e1e6ceeffe
akhier <akhierthedragonhearted@yahoo.com>
2014-05-07 02:23:25 -0400

    End Lesson 1 TwinklebearDev
    Finished following SDL2 tutorial  by TwinklebearDev at:
    http://www.willusher.io/pages/sdl2/

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 
 #include <SDL.h>
 #include <iostream>
+
+
+
 int main(int argc, char **argv){
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
